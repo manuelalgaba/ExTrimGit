b54a553880e43900566245ba23c35c8bef328287
manuelalgaba <manuelalgaba.mr@gmail.com>
2023-12-15 18:16:08 +0100

    Actualizado .java y README

--- Ejercicio4.java
+++ Ejercicio4.java
@@ -4,23 +4,23 @@ public class Ejercicio4 {
     public static void main(String[] args) {
         Random random = new Random();
 
-        int[][] matriz = new int[3][3];
+        int[][] matriz = new int[6][6];//La matriz es de 6*6
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
                 matriz[i][j] = random.nextInt(100);
             }
         }
-        System.out.println("Matriz random:");
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
+        System.out.println("Matriz random:");//Creamos la matriz random
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
                 System.out.print(matriz[i][j] + "\t");
             }
             System.out.println();
         }
-        System.out.println("Matriz Traspuesta:");
-        for (int j = 0; j < 3; j++) {
-            for (int i = 0; i < 3; i++) {
+        System.out.println("Matriz Traspuesta:");//Creamos la traspuesta
+        for (int j = 0; j < 6; j++) {
+            for (int i = 0; i < 6; i++) {
                 System.out.print(matriz[i][j] + "\t");
             }
             System.out.println();
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 1. Copiado a la carpeta un ejercicio de java
 2. Bajado Readme y actualizado
+3. Agrego al ejercicio comentarios y actualizo el codigo para tener una matriz bidimensional 6*6
